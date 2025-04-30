22581c197d12613364875082fac3894f27c1ea05
Marie Muravyeva <marie.muraveva@gmail.com>
2025-04-30 13:52:54 +0600

    fix youtube

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if option == "Инференсим трансляцию с YouTube 🐕‍🦺":
     def get_stream_info(youtube_url):
         ydl_opts = {
             "quiet": True,
-            "format": "230",
+            "format": "233",
             "noplaylist": True,
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
